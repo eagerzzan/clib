9113e22a0068c8b2c738f66ed773294888eee157
TheoSeo <makeitafridaynight@gmail.com>
2014-07-08 00:43:27 +0900

    init function of search

---
+++ search.c
@@ -0,0 +1,35 @@
+
+void sequential_search()
+{
+
+}
+
+void fibonacci_search()
+{
+
+}
+
+void binary_search()
+{
+
+}
+
+void binary_tree_search()
+{
+
+}
+
+void balanced_tree_search()
+{
+
+}
+
+void bubble_search()
+{
+
+}
+
+void radix_search()
+{
+
+}
